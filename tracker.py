ff1fe9237748f46a413a27f833593030e1b389a9
Hallvardd <hallvste@stud.ntnu.no>
2019-02-27 12:24:00 +0100

    made small changes to the Person class and  added Tracker class

---
+++ tracker.py
@@ -0,0 +1,29 @@
+from person import Person
+
+
+class Tracker:
+
+    def __init__(self, threshold:float = 0.1):
+        self.idCounter = 0
+        self.persons = {}
+        self.threshold = threshold
+
+    def add_person(self, position):
+        self.persons[idCounter] = {Person(self.idCounter,position)}
+        self.idCounter += 1
+
+
+    def remove_person(self,personId:int):
+        self.persons.pop(personId)
+
+    def flush_person_dict(self):
+        self.persons = {}
+
+
+    def fill_persondict(self, positions: [(int,int)]):
+        # should be called when making a new list of persons
+        for p in positions:
+            self.add_person(p)
+
+    def update_persondict(self, postition : [(int,int)]):
+        return 0
