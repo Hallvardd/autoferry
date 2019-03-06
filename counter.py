1623abbcd975aef33bcdf13b0769079dc7c294e5
krikjers <krikjers@stud.ntnu.no>
2019-03-06 11:14:08 +0100

    started on counter algorithm

---
+++ counter.py
@@ -0,0 +1,96 @@
+import cv2
+import person
+import numpy as np
+
+def defineCrossingLine():
+	startX = 500
+	endX   = 500
+	startY = 200
+	endY   = 800
+	crossingLine = (startX, startY, endX, endY)
+	return crossingLine
+    #line is in format (xStart,yStart, xEnd,  yEnd)
+
+
+def drawCrossingLine(crossingLine, img):
+	startPoint = (crossingLine[0], crossingLine[1])
+	endPoint   = (crossingLine[2], crossingLine[3])
+	color      = (255,0,0)
+	lineThickness = 2
+
+	cv2.line(img,startPoint,endPoint,color,lineThickness) 
+
+
+def distanceToLineFromCoordinate(line, coordinate):
+	#assume line is in format (xStart,yStart, xEnd,  yEnd)
+	#assume coordinate is a np array in format [x1, y1]
+
+	linePoint1 = np.array([int(line[0]), int(line[1])])
+	linePoint2 = np.array([int(line[2]), int(line[3])])	
+
+
+	distance = abs(np.cross(linePoint2-linePoint1,coordinate-linePoint1)/np.linalg.norm(linePoint2-linePoint1))
+	return distance  #row vector of the position for coordinate
+
+
+
+
+def countPersons(listOfTrackedPersons, previousCount, crossingLine):
+	"""
+	This function is going to be called in each iteration of main loop. 
+	Assume that a person's coordinates are updated.
+	This function returns how many persons that has crossed a line, and updates a person's counted variable.
+	A person is counted if a person's last coordinate is close to the crossing line (defined by a threshold)
+
+	inputs:
+	*listOfTrackedPersons: (list) [person1, person2, person3,....].	Each person has a history of coordinates
+	*previousCount: (int) 3
+	*crossingLine: (tuple) (startX, startY, endX, endY). is the line, when person crosses it the person is counted
+	
+
+	output:
+	*updated number of counted persons
+	*updated personList
+	"""
+
+	threshold = 5 		#number of pixels
+	newCount = previousCount
+
+	for person in listOfTrackedPersons:
+		if (person.counted == False and distanceToLineFromCoordinate(crossingLine, person.get_last_position()) < threshold):
+			person.set_counted(True)
+			newCount += 1
+
+	return newCount, personList
+
+
+
+def testCount():
+	print("testing count")
+	person1 = person.Person((300, 300), 1)
+	person2 = person.Person((400, 400), 2)
+	person3 = person.Person((500, 500), 3)
+
+	personList = [person1, person2, person3]
+	crosslingLine = defineCrossingLine()
+	prevCount = 0
+
+	countPersons(personList, prevCount, crosslingLine)
+
+	print( distanceToLineFromCoordinate(crosslingLine, person1.get_last_position()) )
+
+
+
+testCount()
+
+
+
+
+
+
+
+
+	
+
+
+
